Fixes l2 normalization bug and updaes segmenation script to minimize for loop iterations

masknmf/engine/segmentation.py
@@ -189,6 +189,7 @@ def filter_components_UV(masks, bin_masks, properties, mask_thres, bin_mask_thre
     
     mask_dims, num_masks = masks.shape
     
+    
     ##Normalize masks data structure
     masks = l2_normalize(masks)
     bin_masks = l2_normalize(bin_masks)
@@ -221,7 +222,11 @@ def filter_components_UV(masks, bin_masks, properties, mask_thres, bin_mask_thre
                 max_similarity = product.max()
                 if max_similarity > mask_thres:
                     found_real_match_mask = True
-                
+                    break
+                    
+        if found_real_match_mask:
+            continue
+        
         
         ##Step 2: Check if it is highly similar to currently accepted bin masks
         found_bin_match_mask = False
@@ -235,7 +240,7 @@ def filter_components_UV(masks, bin_masks, properties, mask_thres, bin_mask_thre
                 if max_similarity > bin_mask_thres:
                     found_bin_mask_match = True
     
-        if found_real_match_mask or found_bin_match_mask:
+        if found_bin_match_mask:
             continue
         else:
             keep_masks[mask_index] = 1
@@ -251,5 +256,6 @@ def filter_components_UV(masks, bin_masks, properties, mask_thres, bin_mask_thre
                     accepted_bin_masks = bin_masks_dict[property_val]
                     bin_masks_dict[property_val] = scipy.sparse.hstack([accepted_bin_masks, curr_bin_mask])
     
+    print("we started with {} masks and kept {} masks".format(keep_masks.shape, np.count_nonzero(keep_masks)))
     return keep_masks
     

masknmf/utils/computational_utils.py
@@ -11,14 +11,15 @@ def l2_normalize(scipy_csc_mat, tol = 0.00000001):
     Outputs: 
         norm_mat. scipy.sparse.csc_matrix. Dimensions (d1, d2). Matrix with all columns normalized. 
     '''
-    
     scipy_csc_sq = scipy_csc_mat.multiply(scipy_csc_mat)
     sum_cols = np.asarray(scipy_csc_sq.sum(0))
-    sum_cols[sum_cols<tol] = 1
+    sum_cols[sum_cols < 0.0000000001] = 1
     sum_cols_sqrt = np.sqrt(sum_cols)
     sum_cols_recip = np.reciprocal(sum_cols_sqrt)
-    
-    scipy_csc_normalized = scipy_csc_sq.multiply(sum_cols_recip)
+
+    sum_cols_recip_mat = scipy.sparse.spdiags(sum_cols_recip, 0, scipy_csc_sq.shape[1], scipy_csc_sq.shape[1])
+    scipy_csc_normalized = scipy_csc_mat.dot(sum_cols_recip_mat)
+
     
     return scipy_csc_normalized.tocsc()
 